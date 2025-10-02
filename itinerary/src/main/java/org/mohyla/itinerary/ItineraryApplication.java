0059fd148513257af6ec9f77bee0c6ed22f41191
Valentyn Radchenko <valentyn.radchenko@outlook.com>
2025-10-02 23:18:34 +0200

    Some fixes done, kluster is working

--- itinerary/src/main/java/org/mohyla/itinerary/ItineraryApplication.java
+++ itinerary/src/main/java/org/mohyla/itinerary/ItineraryApplication.java
@@ -1,6 +1,5 @@
 package org.mohyla.itinerary;
 
-//import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
