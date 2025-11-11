7e221857d7144dcec2e6bca90b579bcda8180f9b
Valentyn Radchenko <valentyn.radchenko@outlook.com>
2025-11-11 10:13:27 +0100

    Pr 9 done

--- payments/src/main/java/org/mohyla/payments/domain/persistence/PaymentRepository.java
+++ payments/src/main/java/org/mohyla/payments/domain/persistence/PaymentRepository.java
@@ -3,5 +3,9 @@ package org.mohyla.payments.domain.persistence;
 import org.mohyla.payments.domain.models.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<List<Payment>> findByUserId(Long userId);
 }
